4b8e49c5ae8455c626c09f2f2c76cf679a479a85
shameermyd <shameer.myd@gmail.com>
2022-02-12 13:55:47 +0530

    extend layout in about view page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<div class="content">
+        <div class = "title">
+            @yield("title")
+        </div>
+        <div class ="link">
+            <a href = "/">Home</a>
+            <a href = "/about">About</a>
+            <a href = "/contact">Conact Us</a>
+        </div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<h1>Heloooo</h1>
+    <div class="content">
+        <div class ="link">
+            <a href = "/">Home</a>
+            <a href = "/about">About</a>
+            <a href = "/contact">Conact Us</a>
+        </div>
+    </div>
